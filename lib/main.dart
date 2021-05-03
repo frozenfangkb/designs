42f0c67f285a6df85147835663052aa881b35f08
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-05-03 15:07:43 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: TempHomeScreen());
+  }
+}
+
+class TempHomeScreen extends StatelessWidget {
+  const TempHomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text('Hola mundo'), Text('Hola mundo')],
+    );
+  }
+}
